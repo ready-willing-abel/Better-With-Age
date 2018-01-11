270563523065adb67a5af11821c693c26865d83b
Eetai Ben-Sasson <eeetai@gmail.com>
2018-01-11 11:50:29 -0500

    some progress

--- client/components/ThumbnailShell.js
+++ client/components/ThumbnailShell.js
@@ -1,25 +1,26 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-import {Container, Row, Col} from 'react-grid-system'
+import { Container, Row, Col } from 'react-grid-system'
 
 import SingleCheese from './SingleCheeseThumbnail'
 
 export default class Thumbnails extends Component {
     render() {
         return (
-        <Container fluid style={{ lineHeight: '32px' }}>
-       
-        <Row align="center" style={{ height: '5px' }} debug>
-            <Col debug><SingleCheese/></Col>
-            <Col debug><SingleCheese/></Col>
-            <Col debug><SingleCheese/></Col>
-        </Row>
-        <br />
-        <Row align="center" style={{ height: '75px' }} debug>
-            <Col debug><SingleCheese/></Col>
-            <Col debug><SingleCheese/></Col>
-            <Col debug><SingleCheese/></Col>
-        </Row>
-        </Container>
-    )}
+            <Container fluid style={{ lineHeight: '100px' }}>
+
+                <Row align="center" style={{ height: '50px' }} debug>
+                    <Col debug><SingleCheese /></Col>
+                    <Col debug><SingleCheese /></Col>
+                    <Col debug><SingleCheese /></Col>
+                </Row>
+                <br />
+                <Row align="center" style={{ height: '50px' }} debug>
+                    <Col debug><SingleCheese /></Col>
+                    <Col debug><SingleCheese /></Col>
+                    <Col debug><SingleCheese /></Col>
+                </Row>
+            </Container>
+        )
+    }
 }
