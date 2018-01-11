c92e6bc7ce48d0a6c84c2546e872250ada890992
Vanessa Jiménez <16891417+vjimenez327@users.noreply.github.com>
2018-01-11 13:48:07 -0500

    this had seed data for the db

--- script/seed.js
+++ script/seed.js
@@ -1,36 +1,55 @@
-/**
- * Welcome to the seed file! This seed file uses a newer language feature called...
- *
- *                  -=-= ASYNC...AWAIT -=-=
- *
- * Async-await is a joy to use! Read more about it in the MDN docs:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
- *
- * Now that you've got the main idea, check it out in practice below!
- */
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const { User, Category, Cheese, Purchase } = require('../server/db/models')
+const chalk = require('chalk')
+const chance = require('chance').Chance()
+const Promise = require('bluebird')
 
 async function seed () {
   await db.sync({force: true})
   console.log('db synced!')
-  // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
-  // executed until that promise resolves!
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({name:'Big Sean', email: 'cody@email.com', password: '123', isAdmin:true}),
+    User.create({name:'Murphy Lee', email: 'murphy@email.com', password: '123'}),
+    User.create({name:'Eetai Magic', email: 'eetai@email.com', password: '123'}),
+    User.create({name:'Robin Cello', email: 'robin@email.com', password: '123', isAdmin:true}),
+    User.create({name:'Abel Cheese', email: 'abel@email.com', password: '123'}),
+    User.create({name:'Vanessa Coffee', email: 'vanessa@email.com', password: '123'}),
+    User.create({name:'Jacques Greene', email: 'jacque@email.com', password: '123'}),
+    User.create({name:'Bob Moses', email: 'bob@email.com', password: '123', isAdmin:true})
   ])
-  // Wowzers! We can even `await` on the right-hand side of the assignment operator
-  // and store the result that the promise resolves to in a variable! This is nice!
+
   console.log(`seeded ${users.length} users`)
+
+
+  const cheeses = await Promise.all([
+    Cheese.create({name:'Gouda', category:'', price: '50', quantity:40}),
+    Cheese.create({name:'Cheddar', category:'', price: '21', quantity:20}),
+    Cheese.create({name:'Brie', category:'', price: '14', quantity:15}),
+    Cheese.create({name:'Mozzarella', category:'', price: '61', quantity:30}),
+    Cheese.create({name:'Gruyere', category:'', price: '45', quantity:50})
+  ])
+
+  console.log(`seeded ${cheeses.length} cheeses`)
+
+  const purchases = await Promise.all([
+    Purchase.create({quantity:3, priceAtTimeOfSale:40, ordered: true, userId: 3, cheeseId:5}),
+    Purchase.create({quantity:7, priceAtTimeOfSale:40, ordered: false, userId: 2, cheeseId:1}),
+    Purchase.create({quantity:2, priceAtTimeOfSale:40, ordered: true, userId: 7, cheeseId:3}),
+    Purchase.create({quantity:6, priceAtTimeOfSale:40, ordered: false, userId: 5, cheeseId:2}),
+    Purchase.create({quantity:4, priceAtTimeOfSale:40, ordered: true, userId: 1, cheeseId:4})
+  ])
+
+  console.log(`seeded ${purchases.length} purchases`)
+
+
+
   console.log(`seeded successfully`)
+
+
 }
 
-// Execute the `seed` function
-// `Async` functions always return a promise, so we can use `catch` to handle any errors
-// that might occur inside of `seed`
+
 seed()
   .catch(err => {
     console.error(err.message)
