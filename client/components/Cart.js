b5e40d28859758158a56521e3cfec1d82f0f5f88
Abel McElroy <abel.h.mcelroy@gmail.com>
2018-01-12 15:51:56 -0500

    updated cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { GetPurchasesAll, GetUnorderedPurchasesUser, GetOldPurchasesUser, UpdatePurchase, AddPurchase, DeletePurchase } from '../store/purchases'
-import { List, ListItem } from 'material-ui/List';
 import store from '../store'
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 let dispatch = store.dispatch
+const styleA = {
+  display: "flex"
+}
+const styleB = {
+  flex: 1
+}
+const styleC = {
+  transform: `translate(10px, 50px)`,
+  flex: .5,
+}
+const styleD = {
+  margin: 12
+}
+const styleE = {
+  transform: `translate(15px, 0px)`
+}
+
 
 class Cart extends Component {
   constructor(props) {
@@ -20,26 +44,45 @@ class Cart extends Component {
   render(){
     console.log(this.props)
     if(this.props.unpurchasedOrders) return (
-      <div>
-        <div>I have props</div>
-
-        <List>
+      <div className="cartTableContainer" style={styleA}>
+        <div className="tableContainer-cart" style={styleB}>
+          <Table>
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+              <TableRow>
+                <TableHeaderColumn>Cheese</TableHeaderColumn>
+                <TableHeaderColumn>Quantity</TableHeaderColumn>
+                <TableHeaderColumn>Price</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+            {
+              this.props.unpurchasedOrders.map(cartItem=>{
+                return (
+                  <TableRow>
+                    <TableRowColumn>{cartItem.cheese.name}</TableRowColumn>
+                    <TableRowColumn>{cartItem.quantity}</TableRowColumn>
+                    <TableRowColumn>{'$'+cartItem.cheese.price}</TableRowColumn>
+                  </TableRow>
+                )
+              })
+            }
+            </TableBody>
+          </Table>
+        </div>
+        <div className="cartTableButtonsContainer" style={styleC}>
           {
-            this.props.unpurchasedOrders.map(cartItem=>{
+            this.props.unpurchasedOrders.map(cartItem => {
               return (
-                <div>
-                <ListItem
-                  primaryText={cartItem.cheese.name + '\t' + cartItem.quantity}
-                  rightIcon={
-                    <FloatingActionButton mini={true} onClick={()=>this.props.deltQuantity(cartItem.id,cartItem.quantity + 1)}>
-                      <ContentAdd />
-                    </FloatingActionButton>}
-                />
+                <div className="cartTableButtons" style={styleD}>
+                  <FloatingActionButton mini={true} onClick={() => this.props.deltQuantity(cartItem.id, cartItem.quantity + 1)}>
+                  </FloatingActionButton>
+                  <FloatingActionButton secondary={true} mini={true} onClick={() => this.props.deltQuantity(cartItem.id, cartItem.quantity - 1)} style={styleE}>
+                  </FloatingActionButton>
                 </div>
               )
             })
           }
-        </List>
+        </div>
       </div>
     )
     else return (<div>THIS IS ALSO A DIV</div>)
@@ -62,7 +105,8 @@ function mapDispatchToProps(dispatch) {
       dispatch(GetUnorderedPurchasesUser(userId))
     },
     deltQuantity: (id,value)=>{
-      dispatch(UpdatePurchase(id,{quantity: value}))
+      if(value<1) dispatch(DeletePurchase(id))
+      else dispatch(UpdatePurchase(id,{quantity: value}))
     }
   }
 }
