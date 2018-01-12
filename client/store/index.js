0bcb19f16a0550862dc6a9c0abd094f3ba1feb7c
Robin Luongo <robin.luongo@gmail.com>
2018-01-12 12:36:33 -0500

    fixed for merge

--- client/store/index.js
+++ client/store/index.js
@@ -2,14 +2,9 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-<<<<<<< HEAD
 import user from './user'
 import purchases from './purchases'
-=======
->>>>>>> 1a8c19362cc37f4f5d73fd9e76d941f4c32565ae
 import cheeses from './cheeses'
-import purchases from './purchases'
-import user from './user'
 
 const reducer = combineReducers({user, purchases,  cheeses})
 const middleware = composeWithDevTools(applyMiddleware(
